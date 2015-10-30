dd0952eaeb9ad627e881b948f1a4d1b807d01996
Nam <ruku87@naver.com>
2015-10-30 17:21:07 +0900

    페이지 나누기 회원관리

--- GGGMProject3/src/dao/memberDAO.java
+++ GGGMProject3/src/dao/memberDAO.java
@@ -5,7 +5,6 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-
 import java.io.*;
 
 public class memberDAO {
@@ -36,29 +35,40 @@ public class memberDAO {
 		return d;
 	}
 
-	public static List<memberDTO> getMemberAllData() {
-		SqlSession session = ssf.openSession();
-		List<memberDTO> list = session.selectList("getMemberAllData");
-		session.close();
-		return list;
-	}
-
 	public static List<memberDTO> getADMemberAllData() {
 		SqlSession session = ssf.openSession();
 		List<memberDTO> list = session.selectList("getADMemberAllData");
 		session.close();
 		return list;
 	}
+
 	public static List<memberDTO> getNormalMemberAllData() {
 		SqlSession session = ssf.openSession();
 		List<memberDTO> list = session.selectList("getNormalMemberAllData");
 		session.close();
 		return list;
 	}
+
 	public static List<memberDTO> getAdminMemberAllData() {
 		SqlSession session = ssf.openSession();
 		List<memberDTO> list = session.selectList("getAdminMemberAllData");
 		session.close();
 		return list;
+	}
+
+	public static List<memberDTO> memberListData(Map map) {
+
+		SqlSession session = ssf.openSession();
+		List<memberDTO> list = session.selectList("memberListData", map);
+		session.close();// ¹ÝÈ¯
+		return list;
+	}
+
+	public static int memberTotalPage() {
+		SqlSession session = ssf.openSession();
+		int count = session.selectOne("memberRowCount");
+		int total = (int) (Math.ceil(count / 10.0));
+		session.close();
+		return total;
 	}
 }
--- GGGMProject3/src/model/MemberModel.java
+++ GGGMProject3/src/model/MemberModel.java
@@ -5,13 +5,26 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import dao.*;
 
-
 public class MemberModel implements Model {
 
 	@Override
 	public String handlerRequest(HttpServletRequest req, HttpServletResponse res) throws Exception {
 		
-		List<memberDTO> list=memberDAO.getMemberAllData();		
+		String strPage=req.getParameter("page");
+		if(strPage==null)
+			strPage="1";
+		int curpage=Integer.parseInt(strPage);
+		int rowSize=10;
+		int start=(curpage*rowSize)-(rowSize-1);
+		int end=curpage*rowSize;
+		Map map=new HashMap();
+		map.put("start", start); // #{start}
+		map.put("end", end); // #{end}
+		List<memberDTO> list=
+				memberDAO.memberListData(map);	
+		int totalpage=memberDAO.memberTotalPage();
+		
+		req.setAttribute("totalpage", totalpage);
 		req.setAttribute("list", list);
 		req.setAttribute("jsp", "../Admin/member/member.jsp");
 		return "pages/main/main.jsp";
