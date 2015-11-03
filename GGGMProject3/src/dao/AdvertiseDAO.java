fc41b4cd9fffa62cf26fb54d23024e9ae2b85d87
myungjin <myungjin@choi>
2015-11-03 15:50:42 +0900

    uploading ads

--- GGGMProject3/src/dao/AdvertiseDAO.java
+++ GGGMProject3/src/dao/AdvertiseDAO.java
@@ -33,6 +33,15 @@ public class AdvertiseDAO {
     	return count;
     	
     }
+    
+    public static AdvertiseDTO ad_info(int adno)
+    {
+    	SqlSession session=ssf.openSession();
+    	AdvertiseDTO dto=new AdvertiseDTO();
+    	dto=session.selectOne("ad_info", adno);
+    	session.close();
+    	return dto;
+    }
 
 	
 	
--- GGGMProject3/src/model/Ad_IndexModel.java
+++ GGGMProject3/src/model/Ad_IndexModel.java
@@ -5,6 +5,8 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.AdvertiseDAO;
+import dao.AdvertiseDTO;
 import dao.ReplyBoardDAO;
 import dao.ReplyDTO;
 
@@ -12,13 +14,18 @@ public class Ad_IndexModel implements Model {
 
 	@Override
 	public String handlerRequest(HttpServletRequest req, HttpServletResponse res) throws Exception {
-		int strNo=Integer.parseInt(req.getParameter("adno"));
-		List<ReplyDTO> list = ReplyBoardDAO.replyListData(strNo);
+		int adno=Integer.parseInt(req.getParameter("adno"));
+		System.out.println("adno:"+adno);
+		AdvertiseDTO d=AdvertiseDAO.ad_info(adno);
+		req.setAttribute("adinfo", d);
+		
+		List<ReplyDTO> list = ReplyBoardDAO.replyListData(adno);
 		System.out.println("list size:"+list.size());
 		req.setAttribute("list", list);
-		req.setAttribute("curpage", strNo);
-		req.setAttribute("jsp", "../adview/ad_index.jsp?adno="+strNo);
+		req.setAttribute("curpage", adno);
+		req.setAttribute("jsp", "../adview/ad_index.jsp?adno="+adno);
 		return "pages/main/main.jsp";
 	}
+
 
 }
