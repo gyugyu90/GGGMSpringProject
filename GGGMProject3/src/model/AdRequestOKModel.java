6dc847ff63bb4986e34e0e96c9a530406ad87d73
mutchoi <mutchoi@naver.com>
2015-11-06 17:17:21 +0900

    ad request Form 값 입력 완료 Adrequest table DB 값 쓰는 것 완료 -- 관리자 승인 후, ad table로 옮기는 것 필요.

--- GGGMProject3/src/model/AdRequestOKModel.java
+++ GGGMProject3/src/model/AdRequestOKModel.java
@@ -2,6 +2,7 @@ package model;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.oreilly.servlet.MultipartRequest;
 import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
@@ -40,71 +41,6 @@ public class AdRequestOKModel implements Model {
 		String min_sal= mr.getParameter("min_sal");
 		String max_sal= mr.getParameter("max_sal");
 		String[] interest = mr.getParameterValues("interest");
-		double[] wgt  = new double[7];
-		try {
-			for(int i=0;i<7;i++){
-				wgt[i]=Double.parseDouble(mr.getParameter("weight"+(i+1)));
-			}
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-			//되돌리기
-		}
-		
-		AdrequestDTO d = new AdrequestDTO();
-		
-//		private int adno;===자동증가
-//		private String adid;===불러오기 세션
-//		private String adsubject;
-		d.setAdsubject(adsubject);
-//		private double adtime; === 관리자 증가
-//		private String msg;
-		d.setMsg(msg);
-
-//		private String maj_category;
-		d.setMaj_category(maj_category);
-//		private String sub_category;
-		d.setSub_category(sub_category);
-//		private String url;
-		d.setUrl(url);
-//		private String img;
-		d.setImg(img);
-//		private int ppc;
-		try {
-			d.setPpc(Integer.parseInt(ppc));
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-			//되돌려 주기...
-		}
-		
-//		private int hit; ==== 자동증가
-//		private String data1;
-		
-		String data1="";
-		String data2="";
-		String weight="";
-		
-		int jTotal=0;
-		for(String j:job){
-			jTotal=jTotal+Integer.parseInt(j);
-		}
-		
-		int iTotal=0;
-		for(String i:interest){
-			iTotal=iTotal+Integer.parseInt(i);
-		}
-		
-		data1=sex+"|"+min_age+"|"+addr+"|"+marry+"|"+jTotal+"|"+min_sal+"|"+iTotal;
-		data2=sex+"|"+max_age+"|"+addr+"|"+marry+"|"+jTotal+"|"+max_sal+"|"+iTotal;
-		for(int i=0;i<7;i++){
-			weight=weight+wgt[i];
-			if(i<6)
-				weight=weight+"|";
-		}
-		
-		
-		
-		
-//		AdrequestDTO
 		
 		if(adsubject==null)
 			adsubject="안들어옴.";
@@ -139,10 +75,79 @@ public class AdRequestOKModel implements Model {
 //		if(interest==null)
 //			interest="안들어옴.";
 
-
+		double[] wgt  = new double[7];
+		try {
+			for(int i=1;i<=7;i++){
+				wgt[i-1]=Double.parseDouble(mr.getParameter(("wgt"+i)));
+				System.out.println(wgt[i-1]);
+			}
+		} catch (Exception e) {
+			System.out.println("parsedouble:"+e.getMessage());
+			//되돌리기
+		}
 		
+		AdrequestDTO d = new AdrequestDTO();
+		
+		
+		d.setAdid((String)req.getSession().getAttribute("id"));
+//		private int adno;===자동증가
+//		private String adid;===불러오기 세션
+//		private String adsubject;
+		d.setAdsubject(adsubject);
+//		private double adtime; === 관리자 증가
+//		private String msg;
+		d.setMsg(msg);
 
-		return adsubject;
+//		private String maj_category;
+		d.setMaj_category(maj_category);
+//		private String sub_category;
+		d.setSub_category(sub_category);
+//		private String url;
+		d.setUrl(url);
+//		private String img;
+		d.setImg(img);
+//		private int ppc;
+		try {
+			d.setPpc(Integer.parseInt(ppc));
+		} catch (Exception e) {
+			System.out.println("setppc:"+e.getMessage());
+			//되돌려 주기...
+		}
+		
+//		private int hit; ==== 자동증가
+//		private String data1;
+		
+		String data1="";
+		String data2="";
+		String weight="";
+		
+		int jTotal=0;
+		for(String j:job){
+			jTotal=jTotal+Integer.parseInt(j);
+		}
+		
+		int iTotal=0;
+		for(String i:interest){
+			iTotal=iTotal+Integer.parseInt(i);
+		}
+		
+		data1=sex+"|"+min_age+"|"+addr+"|"+marry+"|"+jTotal+"|"+min_sal+"|"+iTotal;
+		data2=sex+"|"+max_age+"|"+addr+"|"+marry+"|"+jTotal+"|"+max_sal+"|"+iTotal;
+		for(int i=0;i<7;i++){
+			weight=weight+wgt[i];
+			if(i<6)
+				weight=weight+"|";
+		}
+		
+		d.setData1(data1);
+		d.setData2(data2);
+		d.setWeight(weight);
+		
+		
+		AdrequestDAO dao = new AdrequestDAO();
+		dao.adRequestInsert(d);
+		
+		return "main.do";
 		
 
 //		BoardDTO d = new BoardDTO();
